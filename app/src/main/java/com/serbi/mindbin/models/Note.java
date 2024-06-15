b2cbd498156c2382dbd7983bbbfbb42e091e1a59
eipna <Xirveipna@tuta.io>
2024-06-16 01:16:17 +0800

    Added: Note model class

---
+++ app/src/main/java/com/serbi/mindbin/models/Note.java
@@ -0,0 +1,32 @@
+package com.serbi.mindbin.models;
+
+public class Note {
+
+    private String title;
+    private String content;
+    private String status;
+    private String dateCreation;
+
+    public Note(String title, String content, String status, String dateCreation) {
+        this.title = title;
+        this.content = content;
+        this.status = status;
+        this.dateCreation = dateCreation;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getDateCreation() {
+        return dateCreation;
+    }
+}
