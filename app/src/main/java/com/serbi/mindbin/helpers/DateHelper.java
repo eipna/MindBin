d0f9b36ec9469f6573aaa122ca229840a1e8aeae
eipna <Xirveipna@tuta.io>
2024-06-16 01:29:14 +0800

    Update: Simple date to detailed date converter

--- app/src/main/java/com/serbi/mindbin/helpers/DateHelper.java
+++ app/src/main/java/com/serbi/mindbin/helpers/DateHelper.java
@@ -1,5 +1,6 @@
 package com.serbi.mindbin.helpers;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -14,5 +15,19 @@ public class DateHelper {
     public static String getCurrentSimpleDate() {
         String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
         return date;
+    }
+
+    public static String convertSimpleToDetailedDate(String dateInput) {
+        String convertedDate = null;
+        try {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date parsedDate = inputFormat.parse(dateInput);
+
+            SimpleDateFormat outputFormat = new SimpleDateFormat("EEEE, MMMM dd yyyy");
+            convertedDate = outputFormat.format(parsedDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return convertedDate;
     }
 }
