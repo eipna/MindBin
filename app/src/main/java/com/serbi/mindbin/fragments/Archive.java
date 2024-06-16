dff999dad369ea278bfbce4d2331ed3876487695
eipna <Xirveipna@tuta.io>
2024-06-17 03:06:38 +0800

    Update: Archive and Trash recyclerview functionality

--- app/src/main/java/com/serbi/mindbin/fragments/Archive.java
+++ app/src/main/java/com/serbi/mindbin/fragments/Archive.java
@@ -1,66 +1,58 @@
 package com.serbi.mindbin.fragments;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.serbi.mindbin.R;
+import com.serbi.mindbin.adapters.NoteAdapter;
+import com.serbi.mindbin.helpers.DatabaseHelper;
+import com.serbi.mindbin.helpers.DateHelper;
+import com.serbi.mindbin.models.Note;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Archive#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class Archive extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Archive() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Archive.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Archive newInstance(String param1, String param2) {
-        Archive fragment = new Archive();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private View view;
+    private RecyclerView recyclerView;
+    private DatabaseHelper databaseHelper;
+    private NoteAdapter adapter;
+    private ArrayList<Note> notesArrayList;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_archive, container, false);
+        initializeComponents();
+        storeNoteData();
+
+        adapter = new NoteAdapter(getContext(), notesArrayList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return view;
+    }
+
+    private void initializeComponents() {
+        recyclerView = view.findViewById(R.id.recycler_view_note_archive);
+        databaseHelper = new DatabaseHelper(getContext());
+        notesArrayList = new ArrayList<>();
+    }
+
+    private void storeNoteData() {
+        Cursor cursor = databaseHelper.getArchivedNotes();
+        while (cursor.moveToNext()) {
+            notesArrayList.add(new Note(
+                    cursor.getInt(0),
+                    cursor.getString(1), DateHelper.convertSimpleToDetailedDate(cursor.getString(2)),
+                    cursor.getString(3), cursor.getString(4))
+            );
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_archive, container, false);
     }
 }
