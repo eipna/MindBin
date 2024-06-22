f281939d183b5ad386c98d3b62158ef6a3846798
eipna <Xirveipna@tuta.io>
2024-06-22 18:20:03 +0800

    Update: App version name and code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.serbi.mindbin"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.1.2"
+        versionCode = 11
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
