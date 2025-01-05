99146059ee7aec17a703900fd0c36a4699dc1173
Eipna <Xirveipna@tuta.io>
2025-01-05 17:23:06 +0800

    Added application class

---
+++ app/src/main/java/com/eipna/mindbin/MindBinApp.java
@@ -0,0 +1,10 @@
+package com.eipna.mindbin;
+
+import android.app.Application;
+
+public class MindBinApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
