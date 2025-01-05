9995233d28b4c303d1834aa8446a7cf1bb8eed3b
Eipna <Xirveipna@tuta.io>
2025-01-05 17:41:45 +0800

    Added view binding, setup edge-to-edge and toolbar

--- app/src/main/java/com/eipna/mindbin/MainActivity.java
+++ app/src/main/java/com/eipna/mindbin/MainActivity.java
@@ -1,14 +1,34 @@
 package com.eipna.mindbin;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.eipna.mindbin.databinding.ActivityMainBinding;
+import com.google.android.material.shape.MaterialShapeDrawable;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+
+        Drawable drawable = MaterialShapeDrawable.createWithElevationOverlay(this);
+        binding.appBar.setStatusBarForeground(drawable);
+        
+        setSupportActionBar(binding.toolbar);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
