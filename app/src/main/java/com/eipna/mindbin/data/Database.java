128f42e9e77fdae115c5acb63e01e6d6f2a47484
Eipna <Xirveipna@tuta.io>
2025-01-05 22:57:33 +0800

    Added database class

---
+++ app/src/main/java/com/eipna/mindbin/data/Database.java
@@ -0,0 +1,37 @@
+package com.eipna.mindbin.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "mindbin.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_NOTE = "notes";
+    public static final String COLUMN_NOTE_ID = "note_id";
+    public static final String COLUMN_NOTE_TITLE = "title";
+    public static final String COLUMN_NOTE_CONTENT = "content";
+
+    public Database(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createNoteTable = "CREATE TABLE IF NOT EXISTS " + TABLE_NOTE + "(" +
+                COLUMN_NOTE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NOTE_TITLE + " TEXT, " +
+                COLUMN_NOTE_CONTENT + " TEXT)";
+        sqLiteDatabase.execSQL(createNoteTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
+        onCreate(sqLiteDatabase);
+    }
+}
