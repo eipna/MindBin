b5711fc4d69043af33a3dd2a6bd164ed5199c929
Eipna <Xirveipna@tuta.io>
2025-01-06 04:25:27 +0800

    Added adapter class for notes

---
+++ app/src/main/java/com/eipna/mindbin/ui/adapters/NoteAdapter.java
@@ -0,0 +1,66 @@
+package com.eipna.mindbin.ui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.eipna.mindbin.R;
+import com.eipna.mindbin.data.note.Note;
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.textview.MaterialTextView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder>{
+
+    private final Context context;
+    private final ArrayList<Note> list;
+
+    public NoteAdapter(@NotNull Context context, ArrayList<Note> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public NoteAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_note, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteAdapter.ViewHolder holder, int position) {
+        Note note = list.get(position);
+        holder.bind(note);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        MaterialCardView parent;
+        MaterialTextView title;
+        MaterialTextView content;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            parent = itemView.findViewById(R.id.recyclerNoteParent);
+            title = itemView.findViewById(R.id.recyclerNoteTitle);
+            content = itemView.findViewById(R.id.recyclerNoteContent);
+        }
+
+        public void bind(Note note) {
+            title.setText(note.getTitle());
+            content.setText(note.getContent());
+        }
+    }
+}
