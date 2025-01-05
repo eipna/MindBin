be4c8115bae55bf6defa5dc01c1e184d59aeaa66
Eipna <Xirveipna@tuta.io>
2025-01-05 22:50:10 +0800

    Added model class for notes

---
+++ app/src/main/java/com/eipna/mindbin/data/Note.java
@@ -0,0 +1,38 @@
+package com.eipna.mindbin.data;
+
+public class Note {
+
+    private long ID;
+    private String title;
+    private String content;
+
+    public Note() {
+        this.ID = -1;
+        this.title = null;
+        this.content = null;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
