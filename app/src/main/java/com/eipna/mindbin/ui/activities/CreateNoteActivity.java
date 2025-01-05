a5736f407856f24d10f20c9b4f15a6a9c9007a76
Eipna <Xirveipna@tuta.io>
2025-01-05 23:41:23 +0800

    Added share note content

--- app/src/main/java/com/eipna/mindbin/ui/activities/CreateNoteActivity.java
+++ app/src/main/java/com/eipna/mindbin/ui/activities/CreateNoteActivity.java
@@ -1,13 +1,22 @@
 package com.eipna.mindbin.ui.activities;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import com.eipna.mindbin.R;
 import com.eipna.mindbin.databinding.ActivityCreateNoteBinding;
 import com.google.android.material.shape.MaterialShapeDrawable;
+
+import java.util.Objects;
 
 public class CreateNoteActivity extends AppCompatActivity {
 
@@ -33,5 +42,33 @@ public class CreateNoteActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         binding = null;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.create_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.share) showShareIntent();
+        return true;
+    }
+
+    private void showShareIntent() {
+        String noteContent = Objects.requireNonNull(binding.contentInput.getText()).toString();
+        if (noteContent.isEmpty()) {
+            Toast.makeText(this, "Nothing to share here", Toast.LENGTH_SHORT).show();
+        } else {
+           Intent sendIntent = new Intent();
+           sendIntent.setAction(Intent.ACTION_SEND);
+           sendIntent.putExtra(Intent.EXTRA_TEXT, noteContent);
+           sendIntent.setType("text/plain");
+
+           Intent shareIntent = Intent.createChooser(sendIntent, null);
+           startActivity(shareIntent);
+        }
     }
 }
