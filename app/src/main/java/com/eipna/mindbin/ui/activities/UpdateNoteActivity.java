fefb65fb6bb77549930d3789e028e99fdcf07aad
Eipna <Xirveipna@tuta.io>
2025-01-08 12:00:09 +0800

    Appbar and toolbar setup for update note activity class

--- app/src/main/java/com/eipna/mindbin/ui/activities/UpdateNoteActivity.java
+++ app/src/main/java/com/eipna/mindbin/ui/activities/UpdateNoteActivity.java
@@ -1,16 +1,37 @@
 package com.eipna.mindbin.ui.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-import com.eipna.mindbin.R;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.eipna.mindbin.databinding.ActivityUpdateNoteBinding;
+import com.google.android.material.shape.MaterialShapeDrawable;
 
 public class UpdateNoteActivity extends AppCompatActivity {
+
+    private ActivityUpdateNoteBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_note);
+        EdgeToEdge.enable(this);
+        binding = ActivityUpdateNoteBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Drawable drawable = MaterialShapeDrawable.createWithElevationOverlay(this);
+        binding.appBar.setStatusBarForeground(drawable);
+
+        setSupportActionBar(binding.toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
