9a33029c89d751018dc6f2f0a8d0f04ce7d3c547
Eipna <Xirveipna@tuta.io>
2025-01-09 22:28:05 +0800

    Implemented note repository interface in database class

--- app/src/main/java/com/eipna/mindbin/data/Database.java
+++ app/src/main/java/com/eipna/mindbin/data/Database.java
@@ -1,12 +1,20 @@
 package com.eipna.mindbin.data;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class Database extends SQLiteOpenHelper {
+import com.eipna.mindbin.data.note.Note;
+import com.eipna.mindbin.data.note.NoteRepository;
+
+import java.util.ArrayList;
+
+public class Database extends SQLiteOpenHelper implements NoteRepository {
 
     private static final String DATABASE_NAME = "mindbin.db";
     private static final int DATABASE_VERSION = 1;
@@ -33,5 +41,55 @@ public class Database extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
         onCreate(sqLiteDatabase);
+    }
+
+    @Override
+    public void createNote(Note createdNote) {
+        SQLiteDatabase database = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NOTE_TITLE, createdNote.getTitle());
+        values.put(COLUMN_NOTE_CONTENT, createdNote.getContent());
+        database.insert(TABLE_NOTE, null, values);
+        database.close();
+    }
+
+    @Override
+    public void updateNote(Note updatedNote) {
+        SQLiteDatabase database = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NOTE_TITLE, updatedNote.getTitle());
+        values.put(COLUMN_NOTE_CONTENT, updatedNote.getContent());
+        database.update(TABLE_NOTE, values, COLUMN_NOTE_ID + " = ?", new String[]{String.valueOf(updatedNote.getID())});
+        database.close();
+    }
+
+    @Override
+    public void deleteNote(Note deletedNote) {
+        SQLiteDatabase database = getWritableDatabase();
+        database.delete(TABLE_NOTE, COLUMN_NOTE_ID + " = ?", new String[]{String.valueOf(deletedNote.getID())});
+        database.close();
+    }
+
+    @SuppressLint("Range")
+    @Override
+    public ArrayList<Note> getNotes() {
+        SQLiteDatabase database = getReadableDatabase();
+        ArrayList<Note> list = new ArrayList<>();
+        String readNotesQuery = "SELECT * FROM " + TABLE_NOTE;
+
+        @SuppressLint("Recycle")
+        Cursor cursor = database.rawQuery(readNotesQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Note queriedNote = new Note();
+                queriedNote.setID(cursor.getInt(cursor.getColumnIndex(COLUMN_NOTE_ID)));
+                queriedNote.setTitle(cursor.getString(cursor.getColumnIndex(COLUMN_NOTE_TITLE)));
+                queriedNote.setContent(cursor.getString(cursor.getColumnIndex(COLUMN_NOTE_CONTENT)));
+                list.add(queriedNote);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        database.close();
+        return list;
     }
 }
