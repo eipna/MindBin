2c41278eb0d7eda3e48d9addbeba1635bc3ec492
Eipna <Xirveipna@tuta.io>
2025-01-09 22:47:35 +0800

    Created custom item decoration class for notes

---
+++ app/src/main/java/com/eipna/mindbin/ui/adapters/NoteItemDecoration.java
@@ -0,0 +1,27 @@
+package com.eipna.mindbin.ui.adapters;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteItemDecoration extends RecyclerView.ItemDecoration {
+
+    private final int space;
+
+    public NoteItemDecoration(int space) {
+        this.space = space;
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        outRect.bottom = space;
+        outRect.left = space;
+        outRect.right = space;
+
+        if (parent.getChildAdapterPosition(view) == 0) {
+            outRect.top = space;
+        }
+    }
+}
