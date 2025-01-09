4c727f62160e0dc713b3b2ffb31c18fbe7625516
Eipna <Xirveipna@tuta.io>
2025-01-09 13:19:26 +0800

    Added share note content feature in update note activity

--- app/src/main/java/com/eipna/mindbin/ui/activities/UpdateNoteActivity.java
+++ app/src/main/java/com/eipna/mindbin/ui/activities/UpdateNoteActivity.java
@@ -1,14 +1,23 @@
 package com.eipna.mindbin.ui.activities;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.eipna.mindbin.R;
 import com.eipna.mindbin.data.Database;
 import com.eipna.mindbin.databinding.ActivityUpdateNoteBinding;
 import com.google.android.material.shape.MaterialShapeDrawable;
+
+import java.util.Objects;
 
 public class UpdateNoteActivity extends AppCompatActivity {
 
@@ -39,6 +48,34 @@ public class UpdateNoteActivity extends AppCompatActivity {
 
         binding.titleInput.setText(selectedNoteTitle);
         binding.contentInput.setText(selectedNoteContent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.update_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.share) showShareIntent();
+        return true;
+    }
+
+    private void showShareIntent() {
+        String noteContent = Objects.requireNonNull(binding.contentInput.getText()).toString();
+        if (noteContent.isEmpty()) {
+            Toast.makeText(this, "Nothing to share here", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent sendIntent = new Intent();
+            sendIntent.putExtra(Intent.EXTRA_TEXT, noteContent);
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.setType("text/plain");
+
+            Intent shareIntent = Intent.createChooser(sendIntent, null);
+            startActivity(shareIntent);
+        }
     }
 
     @Override
