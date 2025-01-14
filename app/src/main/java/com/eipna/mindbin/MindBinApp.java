cd924bee03c2e0ad7d5d7d8b288757dde1d81aaf
Eipna <Xirveipna@tuta.io>
2025-01-14 21:24:33 +0800

    Load saved application theme on load

--- app/src/main/java/com/eipna/mindbin/MindBinApp.java
+++ app/src/main/java/com/eipna/mindbin/MindBinApp.java
@@ -2,9 +2,26 @@ package com.eipna.mindbin;
 
 import android.app.Application;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.eipna.mindbin.data.enums.Theme;
+import com.eipna.mindbin.util.SharedPreferenceUtil;
+
 public class MindBinApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        SharedPreferenceUtil sharedPreferenceUtil = new SharedPreferenceUtil(this);
+        String themeVal = sharedPreferenceUtil.getString("theme", Theme.get(Theme.SYSTEM));
+
+        if (themeVal.equals(Theme.get(Theme.SYSTEM))) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        } else if (themeVal.equals(Theme.get(Theme.LIGHT))) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else if (themeVal.equals(Theme.get(Theme.DARK))) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else if (themeVal.equals(Theme.get(Theme.BATTERY_SAVING))) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+        }
     }
 }
