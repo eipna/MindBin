9d3c232dd1e78a7f7d069e4a9b5bb600d84b291f
Eipna <Xirveipna@tuta.io>
2025-01-19 18:21:03 +0800

    Created util class for handling date patterns

---
+++ app/src/main/java/com/eipna/mindbin/util/DateUtil.java
@@ -0,0 +1,17 @@
+package com.eipna.mindbin.util;
+
+import android.annotation.SuppressLint;
+
+import com.eipna.mindbin.data.DatePattern;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+    public static String getByPattern(DatePattern pattern, long timeStamp) {
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern.value);
+        return simpleDateFormat.format(new Date(timeStamp));
+    }
+}
