565ee561cc499bb6822f031002eba31677d24d56
Eipna <Xirveipna@tuta.io>
2025-01-21 19:58:14 +0800

    Added opening their respective notes for archive and trash activities

--- app/src/main/java/com/eipna/mindbin/ui/activities/TrashActivity.java
+++ app/src/main/java/com/eipna/mindbin/ui/activities/TrashActivity.java
@@ -1,13 +1,17 @@
 package com.eipna.mindbin.ui.activities;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 
+import com.eipna.mindbin.data.MindBinDatabase;
 import com.eipna.mindbin.data.ViewMode;
 import com.eipna.mindbin.data.note.Note;
 import com.eipna.mindbin.data.note.NoteListener;
@@ -64,9 +68,35 @@ public class TrashActivity extends BaseActivity implements NoteListener {
         binding = null;
     }
 
+    private final ActivityResultLauncher<Intent> updateNoteLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == RESULT_OK) {
+            Intent resultIntent = result.getData();
+            if (resultIntent != null) {
+                Note updatedNote = new Note();
+                updatedNote.setID(resultIntent.getIntExtra(MindBinDatabase.COLUMN_NOTE_ID, -1));
+                updatedNote.setTitle(resultIntent.getStringExtra(MindBinDatabase.COLUMN_NOTE_TITLE));
+                updatedNote.setContent(resultIntent.getStringExtra(MindBinDatabase.COLUMN_NOTE_CONTENT));
+                updatedNote.setLastUpdated(resultIntent.getLongExtra(MindBinDatabase.COLUMN_NOTE_LAST_UPDATED, -1));
+                updatedNote.setState(resultIntent.getIntExtra(MindBinDatabase.COLUMN_NOTE_STATE, -2));
+                noteRepository.update(updatedNote);
+                noteList = new ArrayList<>(noteRepository.getNotesByState(NoteState.TRASH));
+                binding.emptyIndicator.setVisibility(noteList.isEmpty() ? View.VISIBLE : View.GONE);
+                noteAdapter.update(noteList);
+            }
+        }
+    });
+
     @Override
     public void OnNoteClick(int position) {
-
+        Note selectedNote = noteList.get(position);
+        Intent updateNoteIntent = new Intent(getApplicationContext(), UpdateNoteActivity.class);
+        updateNoteIntent.putExtra(MindBinDatabase.COLUMN_NOTE_ID, selectedNote.getID());
+        updateNoteIntent.putExtra(MindBinDatabase.COLUMN_NOTE_TITLE, selectedNote.getTitle());
+        updateNoteIntent.putExtra(MindBinDatabase.COLUMN_NOTE_CONTENT, selectedNote.getContent());
+        updateNoteIntent.putExtra(MindBinDatabase.COLUMN_NOTE_DATE_CREATED, selectedNote.getDateCreated());
+        updateNoteIntent.putExtra(MindBinDatabase.COLUMN_NOTE_LAST_UPDATED, selectedNote.getLastUpdated());
+        updateNoteIntent.putExtra(MindBinDatabase.COLUMN_NOTE_STATE, selectedNote.getState());
+        updateNoteLauncher.launch(updateNoteIntent);
     }
 
     @Override
