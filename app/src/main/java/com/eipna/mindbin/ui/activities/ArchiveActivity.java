4d81244fa48a29ebc3d95fd59a7b24880792d58c
Eipna <Xirveipna@tuta.io>
2025-01-21 19:50:53 +0800

    Added loading of their respective notes for archive and trash pages

--- app/src/main/java/com/eipna/mindbin/ui/activities/ArchiveActivity.java
+++ app/src/main/java/com/eipna/mindbin/ui/activities/ArchiveActivity.java
@@ -2,19 +2,34 @@ package com.eipna.mindbin.ui.activities;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 import com.eipna.mindbin.R;
+import com.eipna.mindbin.data.ViewMode;
+import com.eipna.mindbin.data.note.Note;
+import com.eipna.mindbin.data.note.NoteListener;
+import com.eipna.mindbin.data.note.NoteRepository;
+import com.eipna.mindbin.data.note.NoteState;
 import com.eipna.mindbin.databinding.ActivityArchiveBinding;
+import com.eipna.mindbin.ui.adapters.NoteAdapter;
+import com.eipna.mindbin.ui.adapters.NoteItemDecoration;
 import com.eipna.mindbin.util.SharedPreferenceUtil;
 import com.google.android.material.color.DynamicColors;
 import com.google.android.material.shape.MaterialShapeDrawable;
 
-public class ArchiveActivity extends BaseActivity {
+import java.util.ArrayList;
+
+public class ArchiveActivity extends BaseActivity implements NoteListener {
 
     private ActivityArchiveBinding binding;
+    private NoteRepository noteRepository;
+    private ArrayList<Note> noteList;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +45,36 @@ public class ArchiveActivity extends BaseActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        noteRepository = new NoteRepository(this);
+        noteList = new ArrayList<>(noteRepository.getNotesByState(NoteState.ARCHIVE));
+        noteAdapter = new NoteAdapter(this, this, noteList);
+        binding.emptyIndicator.setVisibility(noteList.isEmpty() ? View.VISIBLE : View.GONE);
+
+        String selectedViewMode = sharedPreferenceUtil.getString("view_mode", ViewMode.LIST.value);
+        if (selectedViewMode.equals(ViewMode.LIST.value)) {
+            binding.noteList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        } else if (selectedViewMode.equals(ViewMode.TILES.value)) {
+            binding.noteList.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+        }
+
+        binding.noteList.addItemDecoration(new NoteItemDecoration(16));
+        binding.noteList.setAdapter(noteAdapter);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         binding = null;
+    }
+
+    @Override
+    public void OnNoteClick(int position) {
+
+    }
+
+    @Override
+    public void OnNoteLongClick(int position) {
+
     }
 }
--- app/src/main/java/com/eipna/mindbin/ui/activities/TrashActivity.java
+++ app/src/main/java/com/eipna/mindbin/ui/activities/TrashActivity.java
@@ -1,20 +1,31 @@
 package com.eipna.mindbin.ui.activities;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
-import com.eipna.mindbin.R;
+import com.eipna.mindbin.data.ViewMode;
+import com.eipna.mindbin.data.note.Note;
+import com.eipna.mindbin.data.note.NoteListener;
+import com.eipna.mindbin.data.note.NoteRepository;
+import com.eipna.mindbin.data.note.NoteState;
 import com.eipna.mindbin.databinding.ActivityTrashBinding;
-import com.eipna.mindbin.util.SharedPreferenceUtil;
-import com.google.android.material.color.DynamicColors;
+import com.eipna.mindbin.ui.adapters.NoteAdapter;
+import com.eipna.mindbin.ui.adapters.NoteItemDecoration;
 import com.google.android.material.shape.MaterialShapeDrawable;
 
-public class TrashActivity extends BaseActivity {
+import java.util.ArrayList;
+
+public class TrashActivity extends BaseActivity implements NoteListener {
 
     private ActivityTrashBinding binding;
+    private NoteRepository noteRepository;
+    private ArrayList<Note> noteList;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +41,36 @@ public class TrashActivity extends BaseActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        noteRepository = new NoteRepository(this);
+        noteList = new ArrayList<>(noteRepository.getNotesByState(NoteState.TRASH));
+        noteAdapter = new NoteAdapter(this, this, noteList);
+        binding.emptyIndicator.setVisibility(noteList.isEmpty() ? View.VISIBLE : View.GONE);
+
+        String selectedViewMode = sharedPreferenceUtil.getString("view_mode", ViewMode.LIST.value);
+        if (selectedViewMode.equals(ViewMode.LIST.value)) {
+            binding.noteList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        } else if (selectedViewMode.equals(ViewMode.TILES.value)) {
+            binding.noteList.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+        }
+
+        binding.noteList.addItemDecoration(new NoteItemDecoration(16));
+        binding.noteList.setAdapter(noteAdapter);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         binding = null;
+    }
+
+    @Override
+    public void OnNoteClick(int position) {
+
+    }
+
+    @Override
+    public void OnNoteLongClick(int position) {
+
     }
 }
