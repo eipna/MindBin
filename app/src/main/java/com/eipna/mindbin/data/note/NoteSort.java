673bf8ec6c233f5a1ab1e976ebc7f423c6cb2d9b
Eipna <Xirveipna@tuta.io>
2025-02-28 21:36:39 +0800

    Simplified note sort enum

--- app/src/main/java/com/eipna/mindbin/data/note/NoteSort.java
+++ app/src/main/java/com/eipna/mindbin/data/note/NoteSort.java
@@ -1,34 +1,21 @@
 package com.eipna.mindbin.data.note;
 
-import java.util.Comparator;
-
 public enum NoteSort {
-    TITLE_ASCENDING("title_ascending", Note.SORT_TITLE_ASCENDING),
-    TITLE_DESCENDING("title_descending", Note.SORT_TITLE_DESCENDING),
-    DATE_CREATED_LATEST("date_created_latest", Note.SORT_DATE_CREATED_LATEST),
-    DATE_CREATED_OLDEST("date_created_oldest", Note.SORT_DATE_CREATED_OLDEST),
-    LAST_UPDATED_LATEST("last_updated_latest", Note.SORT_LAST_UPDATED_LATEST),
-    LAST_UPDATED_OLDEST("last_updated_oldest", Note.SORT_LAST_UPDATED_OLDEST);
+
+    TITLE("sort_title"),
+    DATE_CREATED("sort_date_created"),
+    LAST_UPDATED("sort_last_updated"),
+    ASCENDING("sort_ascending"),
+    DESCENDING("sort_descending");
 
     public static final NoteSort[] sorts;
-    public final String NAME;
-    public final Comparator<Note> ORDER;
+    public final String SORT;
 
     static {
         sorts = values();
     }
 
-    NoteSort(String name, Comparator<Note> order) {
-        this.NAME = name;
-        this.ORDER = order;
-    }
-
-    public static NoteSort getSort(String selectedSort) {
-        for (NoteSort sort : sorts) {
-            if (selectedSort.equals(sort.NAME)) {
-                return sort;
-            }
-        }
-        return null;
+    NoteSort(String sort) {
+        this.SORT = sort;
     }
 }
